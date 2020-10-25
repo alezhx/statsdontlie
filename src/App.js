6fe7e8fcdcd4769610395484673d128e15027045
victor lin <v.lin01@gmail.com>
2020-10-25 15:00:30 -0400

    update packages

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import HomepageLayout from './views/Homepage'
 import PlayerStats from './views/PlayerStats'
 import axios from 'axios';
 
-
-
-var gapi = require('gapi');
-
 class App extends Component {
   constructor(props) {
     super(props);
